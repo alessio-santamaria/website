17bcd7fdb6070e5171551398626796e0be6c8f24
Alessio <49405560+rexitaliae@users.noreply.github.com>
2022-04-15 16:23:53 +0200

    updated quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,6 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-        distDir: 'docs',
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
